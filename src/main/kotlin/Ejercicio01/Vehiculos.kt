d52edcd2fbfc88fc10e28fcb6f97f61161c63fc2
mcas2 <mcas2@tutanota.com>
2021-10-22 10:15:08 +0200

    Kotlin más o menos entendido

--- src/main/kotlin/Ejercicio01/Vehiculos.kt
+++ src/main/kotlin/Ejercicio01/Vehiculos.kt
@@ -1,17 +1,15 @@
 package Ejercicio01
 
 
-data class Vehiculo (var numBastidor: Int, var matricula: String?, var tipoCombustible: String,
-                    var precio: Double) {
-}
-class VehiculoComercial (var empresa : String, var ocupantes : Int?, var carga : Int?): Vehiculo(
-            0, "43123JDF", "Gasolina", 3000.0)
-    {
-}
+open class Vehiculo (var numBastidor: Int, var matricula: String?, var tipoCombustible: String,
+                    var precio: Double)
 
+class VehiculoComercial (numBastidor: Int, matricula: String?, tipoCombustible: String, precio: Double,
+                         var empresa : String, var ocupantes : Int?, var carga : Int?):
+    Vehiculo(numBastidor, matricula, tipoCombustible, precio)
 
 fun main () {
-    var coche1 : Vehiculo = Vehiculo (2141, "6060DCK", "Gasolina", 5000.0)
+    var coche1 = Vehiculo (2141, "6060DCK", "Gasolina", 5000.0)
 
     println(coche1)
     println("El coche con matrícula ${coche1.matricula} ha subido 100 euros de precio: ")
@@ -21,9 +19,14 @@ fun main () {
     coche1.precio = subirPrecio(coche1.precio)
     println(coche1)
 
-    var cocheCom1 : VehiculoComercial = VehiculoComercial("Coprinsa",50, 4000)
+    var cocheCom1 : VehiculoComercial = VehiculoComercial(2, "2315adf","Gasolina", 3000.0,
+        "Coprinsa", 400, 4000)
     println("Primer vehículo privado: ")
     println(cocheCom1)
+
+    var a = 17
+    println(a.inv())     //SUMAR 1 y PONERLO NEGATIVO
+    println(a.shr(2))
 }
 
 fun subirPrecio (precio: Double, subida : Double = 200.0) : Double {
